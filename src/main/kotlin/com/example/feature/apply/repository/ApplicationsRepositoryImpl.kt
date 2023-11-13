9061a01fde31278e5e0040d270c7ce16496db05a
eve00 <64850924+eve00@users.noreply.github.com>
2023-11-13 14:01:44 +0900

    修正：Responseクラス作成

--- src/main/kotlin/com/example/feature/apply/repository/ApplicationsRepositoryImpl.kt
+++ src/main/kotlin/com/example/feature/apply/repository/ApplicationsRepositoryImpl.kt
@@ -1,18 +1,34 @@
 package com.example.feature.apply.repository
 
+import com.example.base.http.ExceptionHandler
 import com.example.feature.apply.service.ApplicationApiService
+import com.example.util.BaseResponse
+import com.example.util.SuccessResponse
+import io.ktor.http.*
 
 class ApplicationsRepositoryImpl(
 
     private val applicationApiService: ApplicationApiService,
+    private val exceptionHandler: ExceptionHandler
+
 ) : ApplicationsRepository {
-    // TODO: exception handling
-    override suspend fun createApplication(userId: String, courseId: String): Boolean {
-        return applicationApiService.createApplication(userId, courseId)
+    override suspend fun createApplication(userId: String, courseId: String): BaseResponse<Any> {
+        val isCreated = applicationApiService.createApplication(userId, courseId)
+        return if (isCreated) {
+            return SuccessResponse(statusCode = HttpStatusCode.Created)
+        } else {
+            throw exceptionHandler.respondWithSomethingWentWrongException()
+
+        }
     }
 
-    override suspend fun deleteApplication(applicationId: String): Boolean {
-        return applicationApiService.deleteApplication(applicationId)
+    override suspend fun deleteApplication(applicationId: String): BaseResponse<Any> {
+        val isDeleted = applicationApiService.deleteApplication(applicationId)
+        return if (isDeleted) {
+            SuccessResponse(statusCode = HttpStatusCode.OK)
+        } else {
+            throw exceptionHandler.respondWithSomethingWentWrongException()
+        }
     }
 
 }
